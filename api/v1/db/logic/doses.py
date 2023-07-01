63389a62fed862d0aa48020ed48e940c409996e3
Franco <franco+github@coloccini.com.ar>
2023-06-30 22:23:46 -0300

    Doses query building modification.

--- api/v1/db/logic/doses.py
+++ api/v1/db/logic/doses.py
@@ -5,28 +5,25 @@ from v1.db.helpers import clean_query
 def get_doses(dict_url: dict) -> list[Dose] | None:
     """
     Search doses in database.
+    Params considered: dropper_id, dropper_name, application_datetime, start, end
     """
-    query = clean_query(dict_url)
-    query.update({"$unwind": "$doses"})
-    query.update({"$match": {}})
-    if "dropper_id" in query:
-        query["$match"].update({"doses.dropper_id": query["dropper_id"]})
-        del(query["dropper_id"])
-    if "dropper_name" in query and query["dropper_name"]:
-        print(f"dropper_name: {query['dropper_name']}")
-        query["$match"].update({"name": query["dropper_name"]})
-        del(query["dropper_name"])
-    if "application_datetime" in query:
-        query["$match"].update({"doses.application_datetime": query["application_datetime"]})
-        del(query["application_datetime"])
-    if "end" in query or "start" in query:
+    dict_url = clean_query(dict_url)
+    query = {
+        "$unwind": "$doses",
+        "$match": {}
+    }
+    if "dropper_id" in dict_url:
+        query["$match"].update({"doses.dropper_id": dict_url["dropper_id"]})
+    if "dropper_name" in dict_url and dict_url["dropper_name"]:
+        query["$match"].update({"name": dict_url["dropper_name"]})
+    if "application_datetime" in dict_url:
+        query["$match"].update({"doses.application_datetime": dict_url["application_datetime"]})
+    if "end" in dict_url or "start" in dict_url:
         query["$match"].update({"doses.application_datetime": {}})
-        if "start" in query:
-            query["$match"]["doses.application_datetime"].update({"$gte": query["start"]})
-            del(query["start"])
-        if "end" in query:
-            query["$match"]["doses.application_datetime"].update({"$lte": query["end"]})
-            del(query["end"])
+        if "start" in dict_url:
+            query["$match"]["doses.application_datetime"].update({"$gte": dict_url["start"]})
+        if "end" in dict_url:
+            query["$match"]["doses.application_datetime"].update({"$lte": dict_url["end"]})
     query.update({"$project": {"_id": 0, "doses": 1}})
     doses_db = db_client.droppers.aggregate([{k: v} for k,v in query.items()])
     return [Dose.parse_obj(dose_db["doses"]) for dose_db in doses_db] if doses_db else None
