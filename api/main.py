4505daa402690064d1669668dc7abfe528ea6639
Franco <franco+github@coloccini.com.ar>
2023-07-23 20:30:51 -0300

    Possibility of running dev server from main.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from v1.routers import droppers, doses
 
 app = FastAPI()
@@ -10,3 +11,6 @@ app.include_router(doses.router)
 @app.get("/")
 async def root():
     return "API v1"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
