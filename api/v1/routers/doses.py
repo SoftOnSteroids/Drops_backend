894dc55871931bf3d902a3e2257ef9d723f4c5dc
Franco <franco+github@coloccini.com.ar>
2023-08-05 19:04:49 -0300

    Backend modified to be compatible with with python3.9 to upload to Deta cloud server.

--- api/v1/routers/doses.py
+++ api/v1/routers/doses.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from bson import ObjectId
 from fastapi import APIRouter, status, HTTPException, Query
-from typing import Annotated
+from typing import Annotated, Optional, Union
 from datetime import datetime
 
 from v1.db.models.dose import Dose
@@ -12,14 +12,14 @@ router = APIRouter(prefix="/doses",
                    tags=["doses"],
                    responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado."}})
 
-@router.get("/", response_model= list[Dose] | None)
-async def f_doses(dropper_id: Annotated[str | None, Query()] = None,
-                  dropper_name: Annotated[str | None, Query()] = None,
-                  application_datetime: Annotated[datetime | None, Query()] = None,
-                  place_apply: Annotated[int | None, Query()] =None,
-                  start: Annotated[datetime | None, Query()] = None,
-                  end: Annotated[datetime | None, Query()] = None
-                     ) -> list[Dose] | None:
+@router.get("/", response_model= Optional[list[Dose]])
+async def f_doses(dropper_id: Annotated[Optional[str], Query()] = None,
+                  dropper_name: Annotated[Optional[str], Query()] = None,
+                  application_datetime: Annotated[Optional[datetime], Query()] = None,
+                  place_apply: Annotated[Optional[int], Query()] =None,
+                  start: Annotated[Optional[datetime], Query()] = None,
+                  end: Annotated[Optional[datetime], Query()] = None
+                     ) -> Optional[list[Dose]]:
     return DoseHelper.get_doses({
         "dropper_id": dropper_id,
         "dropper_name": dropper_name,
@@ -29,7 +29,7 @@ async def f_doses(dropper_id: Annotated[str | None, Query()] = None,
         "end": end})
 
 @router.delete("/", response_model= HTTPStatus, status_code=status.HTTP_200_OK)
-async def f_delete_dose(dose: Dose) -> HTTPStatus | HTTPException:
+async def f_delete_dose(dose: Dose) -> Union[HTTPStatus, HTTPException]:
     if dose.dropper_id:
         if db_client.droppers.update_one({
             "_id": ObjectId(dose.dropper_id),
